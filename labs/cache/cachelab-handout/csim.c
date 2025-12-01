2832a41a15e4960143d5f0684e3097af37a1de02
sam_lappy <a@g.com>
2025-12-01 14:04:55 +0530

    tokenized the traces and made cache structure

--- labs/cache/cachelab-handout/csim.c
+++ labs/cache/cachelab-handout/csim.c
@@ -2,14 +2,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "cachelab.h"
 
 typedef struct {
-    
+    int valid;
+    unsigned int setindex;
 }cacheLine;
 
-FILE* parser_construct(char* filename);
+FILE* fileopen(char* filename);
+cacheLine** make_cache(sets, lines, blocksize);
 void parse(FILE* fileptr, int verbose);
+void free_cache(cacheLine** cache, int sets);
 
 int main(int argc, char* argv[])
 {
@@ -44,7 +48,7 @@ int main(int argc, char* argv[])
             case 't':
                 t = 1;
                 trace = optarg;
-                printf("tracefile:- %s\n", trace);
+                // printf("tracefile:- %s\n", trace);
                 break;
 
             case ':':
@@ -63,15 +67,20 @@ int main(int argc, char* argv[])
         exit(-1);
     }
     if (!t){printf("-t flag missing\n"); exit(-1);}
+    int sets = 1 << s;
+    int lines = 1 << E;
+    //  int blocksize = 1 << b; 
+    cacheLine** cache = (sets, lines);
 
-    FILE* tracefile = parser_construct(trace);
-    parse(tracefile, verbose);;
+    FILE* tracefile = fileopen(trace);
+    parse(tracefile, verbose); // it should return the total hits, misses and evictions 
 
     printSummary(0, 0, 0);
+    free_cache(cache, sets);
     return 0;
 }
 
-inline FILE* parser_construct(char* filename)
+inline FILE* fileopen(char* filename)
 {
     FILE* fileptr = fopen(filename, "r");
     if (fileptr == NULL) 
@@ -83,16 +92,58 @@ inline FILE* parser_construct(char* filename)
     return fileptr;
 }
 
+cacheLine** make_cache(sets, lines)
+{
+    cacheLine** cache = malloc(sets * sizeof(cacheLine*));
+    for(int i = 0; i < sets; i++)
+    {
+        cache[i] = malloc(lines * sizeof(cacheLine));
+        // initialize valid bits to 0
+        for(int j = 0; j < lines; j++)
+        {
+            cache[i][j].valid = 0;
+        }
+    }
+    
+    return cache;
+}
+
 void parse(FILE* fileptr, int verbose)
 {
     size_t buffsize = 30;
     char buffer[buffsize];
+    char copy[buffsize];  
     while(fgets(buffer, buffsize, fileptr) != NULL)
     {
-        printf("Line: %s", buffer);
+        strcpy(copy, buffer);
+
+        char* op = strtok(copy, " ,  \n");
+        char* addrstr = strtok(NULL, " ,  \n");
+        char* datasize = strtok(NULL, " ,  \n"); 
+        // printf("op: %s, addr: %s, datasize: %s\n", op, addr, datasize);
+
+        unsigned long addr = strtoul(addrstr, NULL, 16);
+        // now make the checking and mapping and cache logic
+
+        
     }
 
     fclose(fileptr);
+}
+
+
+void free_cache(cacheLine** cache, int sets)
+{
+/*
+free(cache[i]) frees the entire array of cache lines for set i.
+
+There is no need to loop over each line, because they are not individually malloc’d.
+*/
+    for(int i = 0; i < sets; i++)
+    {
+        free(cache[i]);
+    }
+    free(cache);
 }
 
 
